f1d452225a4b7034709fc244265ca30b788068d5
a1111 <diegoatg02@gmail.com>
2024-09-23 23:19:11 -0600

    Impresión de las posibles combinaciones para checar que se cumpla con 2^5-1

--- main.py
+++ main.py
@@ -2,8 +2,17 @@
 import pandas as pd
 import optuna
 import itertools
+
 # Falta crear el backtest y utils para importarlos aquí
 
-# 1. Cargar los Datos
+# Cargar los Datos
 train_data = pd.read_csv('data/aapl_5m_train.csv').dropna()
 
+# Combinaciones de indicadores 2^5-1 = 30 combinaciones
+all_indicators = ["RSI", "Bollinger bands", "MACD", "ATR", "SMA"]
+all_combinations = []
+for r in range(1, len(all_indicators) + 1):
+    combinations = itertools.combinations(all_indicators, r)
+    all_combinations.extend(combinations)
+
+print(f"Total de combinaciones a probar: {len(all_combinations)}")
